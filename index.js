b423832bdc382fe4c78d0159a1b38c69848e9adc
刘杭 <callcter@diancangstudio.com>
2019-06-25 16:50:44 +0800

    js封装

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter, DeviceEventEmitter } from 'react-native'
 
-const { RNScreenshotcatch } = NativeModules;
+let screenShotEmitter = undefined
 
-export default RNScreenshotcatch;
+class RNScreenshotcatchUtil {
+  static startListener(callback){
+    const module = NativeModules.RNScreenshotcatch
+    screenShotEmitter && screenShotEmitter.removeAllListeners('Screenshotcatch')
+    screenShotEmitter = Adapter.isIOS ? new NativeEventEmitter(module) : DeviceEventEmitter
+    screenShotEmitter.addListener('Screenshotcatch', (data) => {
+      if(callback){
+        callback(data)
+      }
+    })
+    module.startListener()
+    return screenShotEmitter
+  }
+
+  static stopListener () {
+    screenShotEmitter && screenShotEmitter.removeAllListeners('Screenshotcatch')
+    const screenShotEmitter = NativeModules.RNScreenshotcatch
+    return screenShotEmitter.stopListener()
+  }
+
+  static hasNavigationBar(){
+    if(!Adapter.isIOS){
+      screenShotEmitter && screenShotEmitter.removeAllListeners('Screenshotcatch')
+      const screenShotEmitter = NativeModules.RNScreenshotcatch
+      return screenShotEmitter.hasNavigationBar()
+    }else{
+      return false
+    }
+  }
+
+}
+
+export default RNScreenshotcatchUtil
